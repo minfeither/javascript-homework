5c4ae61ffd096101407e2f33f4580188e77ede00
minfeither <60680254+minfeither@users.noreply.github.com>
2021-01-16 18:16:29 +0630

    home-work

---
+++ app.js
@@ -0,0 +1,22 @@
+var mark=90;
+
+if (mark >=0 && mark <40) {
+
+    console.log('Fail The Exam!!!!'); 
+
+    }else if(mark >= 40 && mark <65){
+
+        console.log('Pass The Exam!');
+
+    }else if (mark >=65 && mark <80) {
+
+        console.log('Credit The Exam!');
+        
+    }else if (mark >=80 && mark <=100) {
+        
+        console.log('Oudstanding!!!!!!');
+    }else{
+
+        console.log('Find The Error!');
+    }
+
